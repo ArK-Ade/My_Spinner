6cac61730ffe6a6648e5e0b4a3788e40f437fe53
Anthony ADELAIDE <anthony.adelaide@orange.fr>
2022-02-08 14:11:20 +0100

    adding another planet to spinner

--- app/src/main/java/com/example/myspinner/MainActivity.java
+++ app/src/main/java/com/example/myspinner/MainActivity.java
@@ -1,17 +1,35 @@
 package com.example.myspinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Context mContext;
+    private Activity mActivity;
+
+    private CoordinatorLayout mCLayout;
+    private TextView mTextView;
+    private Spinner mSpinner;
+    private ArrayAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = getApplicationContext();
+        mActivity = MainActivity.this;
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -21,5 +39,19 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View arg1, int pos,long id) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        String str = (String) parent.getItemAtPosition(pos);
+        textView.setText(str);
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+
     }
 }
